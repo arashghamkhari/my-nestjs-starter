68d343f8fb74ca969ff35e7f6df3b58283f49070
Arash Ghamkhari <dev@ghamkhari.com>
2024-09-24 12:33:21 +0330

    feat: nested class-validator support

--- src/core/api/filters/exceptions.filter.ts
+++ src/core/api/filters/exceptions.filter.ts
@@ -1,6 +1,6 @@
 import { ArgumentsHost, BadRequestException, Catch } from '@nestjs/common';
 import { BaseExceptionFilter } from '@nestjs/core';
-import { captureException } from '@sentry/nestjs';
+import { captureException, captureMessage } from '@sentry/nestjs';
 
 @Catch()
 export class AllExceptionsFilter extends BaseExceptionFilter {
@@ -26,13 +26,21 @@ export class AllExceptionsFilter extends BaseExceptionFilter {
     // validator errors
     let unknownValidationErrorOccur = false;
     if (Array.isArray(e.response?.message) && e.response?.message.length > 0) {
-      for (const msg of e.response.message) {
+      for (let i = 0; i < e.response.message.length; i++) {
+        let msg = e.response.message[i];
+
+        if (msg.indexOf('.EXC_') > -1)
+          msg = e.response.message[i] = msg.replace(/^.*\.(EXC_.*)$/, '$1'); // It's nested validator
+
         if (!unknownValidationErrorOccur && !msg.startsWith('EXC_'))
           unknownValidationErrorOccur = true;
       }
 
       if (unknownValidationErrorOccur) {
-        captureException(e);
+        captureMessage('Unknown validator error occur', {
+          extra: { messages: e.response.message },
+        });
+
         return res.setHeader('EXC', 'EXC_BAD_REQUEST').end();
       }
 
