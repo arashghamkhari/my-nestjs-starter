61ae1cee8868fd95518cb4901e65a7acb1223299
Arash Ghamkhari <dev@ghamkhari.com>
2024-09-24 12:26:24 +0330

    feat: array support for purify, add makeUniqueItems method

--- src/libraries/purify.library.ts
+++ src/libraries/purify.library.ts
@@ -3,13 +3,44 @@ import { JSDOM } from 'jsdom';
 import { isString } from '@nestjs/common/utils/shared.utils.js';
 
 export class Purify {
-  public static XSSProtector(value: string) {
+  /**
+   * @param value
+   * @param each Specifies if value is an array and each of its items must be protected.
+   * @constructor
+   */
+  public static XSSProtector(value: string | string[], each: boolean = false) {
+    if (each) {
+      if (!Array.isArray(value)) return Purify.XSSProtector(value);
+
+      return value.map((val) => Purify.XSSProtector(val));
+    }
+
     return isString(value)
       ? DOMPurify(new JSDOM('').window).sanitize(value)
       : value;
   }
 
-  public static trimExtraSpaces(value: string) {
+  /**
+   * @param value
+   * @param each Specifies if value is an array and each of its items must be protected.
+   * @constructor
+   */
+  public static trimExtraSpaces(
+    value: string | string[],
+    each: boolean = false,
+  ) {
+    if (each) {
+      if (!Array.isArray(value)) return Purify.XSSProtector(value);
+
+      return value.map((val) => Purify.trimExtraSpaces(val));
+    }
+
     return isString(value) ? value.trim().replace(/\s+/g, ' ') : value;
+  }
+
+  public static makeUniqueItems(value: unknown[]): unknown[] {
+    if (!Array.isArray(value)) return value;
+
+    return [...new Set(value)];
   }
 }
