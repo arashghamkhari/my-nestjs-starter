eed46d394181e2dee871140070feab3520876904
Arash Ghamkhari <dev@ghamkhari.com>
2024-09-26 12:43:40 +0330

    fix: merge persian and arabic utility

--- src/utilities/persian.utility.ts
+++ src/utilities/persian.utility.ts
@@ -25,6 +25,34 @@ export class PersianUtility {
     '9',
   ];
 
+  private static readonly ARABIC_PERSIAN_CHAR_MAP = {
+    ك: 'ک',
+    دِ: 'د',
+    بِ: 'ب',
+    زِ: 'ز',
+    ذِ: 'ذ',
+    شِ: 'ش',
+    سِ: 'س',
+    ى: 'ی',
+    ي: 'ی',
+    ة: 'ه',
+    ؤ: 'و',
+    ئ: 'ی',
+    أ: 'ا',
+    إ: 'ا',
+    آ: 'ا',
+    '١': '۱',
+    '٢': '۲',
+    '٣': '۳',
+    '٤': '۴',
+    '٥': '۵',
+    '٦': '۶',
+    '٧': '۷',
+    '٨': '۸',
+    '٩': '۹',
+    '٠': '۰',
+  };
+
   public static toLatinNumbers(str: string): string {
     return str.replace(/[۰-۹]/g, (char) => {
       return PersianUtility.LATIN_NUMBERS[
@@ -39,5 +67,12 @@ export class PersianUtility {
         PersianUtility.LATIN_NUMBERS.indexOf(char)
       ];
     });
+  }
+
+  public static fromArabic(str: string): string {
+    return str.replace(
+      /ك|دِ|بِ|زِ|ذِ|شِ|سِ|ى|ي|آ|ئ|ة|ؤ|إ|أ|١|٢|٣|٤|٥|٦|٧|٨|٩|٠/gi,
+      (x: string) => PersianUtility.ARABIC_PERSIAN_CHAR_MAP[x],
+    );
   }
 }
