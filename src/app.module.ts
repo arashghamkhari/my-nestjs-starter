6b8040e45d0466cbd97fd58f79cbf2a0f91c1238
Arash Ghamkhari <dev@ghamkhari.com>
2024-09-26 23:22:15 +0330

    fix: typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { SampleModule } from './modules/sample/sample.module';
 import { AppConfig } from './configs/app.config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { TypeormPostgresConfig } from './core/typeorm/configs/typeorm-postgres.config';
 
 @Module({
-  imports: [SampleModule],
+  imports: [
+    TypeOrmModule.forRoot(new TypeormPostgresConfig() as TypeOrmModuleOptions),
+    SampleModule,
+  ],
   controllers: [],
   providers: [AppConfig],
 })
